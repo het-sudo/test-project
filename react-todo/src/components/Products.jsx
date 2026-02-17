09ec4068b81e7eea8d5a679f130968da6092d871
het-sudo <het@voicenova.ai>
2026-02-17 12:55:09 +0530

    deleted

--- react-todo/src/components/Products.jsx
+++ react-todo/src/components/Products.jsx
@@ -1,58 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Products() {
-  const products=[
-    {id:1,name:"laptop",category:"electronics",price:200},
-    {id:2,name:"phone",category:"electronics",price:2000},
-    {id:3,name:"shirt",category:"clothing",price:20000},
-  ]
+  const products = [
+    { id: 1, name: "laptop", category: "electronics", price: 200 },
+    { id: 2, name: "phone", category: "electronics", price: 2000 },
+    { id: 3, name: "shirt", category: "clothing", price: 20000 },
+  ];
 
-  const [category,setcategory]=useState("all")
-  const [sort,setsort]=useState("none")
-  
-  let filtered=[...products]
-  
-  if(filtered!=='all')
+  const [category, setcategory] = useState("all");
+  const [sort, setsort] = useState("none");
 
-    filtered=filtered.filter(p=>p.category===category)
+  let filtered = [...products];
 
-  if(sort==='low')
-    filtered.sort((a,b)=>a.price-b.price)
+  if (category !== "all")
+    filtered = filtered.filter((p) => p.category === category);
 
-  else if(sort==='high')
-    filtered.sort((a,b)=>b.price-a.price)
+  if (sort === "low") filtered.sort((a, b) => a.price - b.price);
+  else if (sort === "high") filtered.sort((a, b) => b.price - a.price);
   return (
     <div>
-     <h1>Products Filter</h1>
-    <select value={category} onChange={(e)=>setcategory(e.target.value)} name="" id="">
-  <option value="all">All</option>
-  <option value="electronics">Electronics</option>
-  <option value="clothing">clothing</option>
+      <h1>Products Filter</h1>
+      <select
+        value={category}
+        onChange={(e) => setcategory(e.target.value)}
+        name=""
+        id=""
+      >
+        <option value="all">All</option>
+        <option value="electronics">Electronics</option>
+        <option value="clothing">clothing</option>
+      </select>
 
-    </select>
+      <select
+        value={sort}
+        onChange={(e) => setsort(e.target.value)}
+        name=""
+        id=""
+      >
+        <option value="none">none</option>
+        <option value="high">desc</option>
+        <option value="low">asc</option>
+      </select>
 
-    <select value={sort} onChange={(e)=>setsort(e.target.value)} name="" id="">
-  <option value="none">none</option>
-  <option value="high">desc</option>
-  <option value="low">asc</option>
-
-    </select>
-
-    
-
-{
-  filtered.length===0?
-  (<>No products</> ): (
-   filtered.map((p)=>
-  <li key={p.id}>{p.name} - {p.price}</li>
-  )
-  )
-}
-     
-
-
+      {filtered.length === 0 ? (
+        <>No products</>
+      ) : (
+        filtered.map((p) => (
+          <li key={p.id}>
+            {p.name} - {p.price}
+          </li>
+        ))
+      )}
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
